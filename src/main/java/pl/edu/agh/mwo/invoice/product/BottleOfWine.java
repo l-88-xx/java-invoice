8560a754d8a7f374aaab8909440d66c6ced768c1
Lila <lila.88.xx@gmail.com>
2026-05-10 16:32:08 +0200

    Tests - ProductTest (wine, fuel)

---
+++ src/main/java/pl/edu/agh/mwo/invoice/product/BottleOfWine.java
@@ -0,0 +1,6 @@
+package pl.edu.agh.mwo.invoice.product;
+
+public class BottleOfWine {
+
+
+}
--- src/test/java/pl/edu/agh/mwo/invoice/product/ProductTest.java
+++ src/test/java/pl/edu/agh/mwo/invoice/product/ProductTest.java
@@ -53,4 +53,33 @@ public class ProductTest {
     public void testProductWithNegativePrice() {
         new TaxFreeProduct("Mandarynki", new BigDecimal("-1.00"));
     }
+
+    @Test
+    public void testBottleOfWinePriceWithTaxAndExcise() {
+        Product wine = new BottleOfWine("Merlot", new BigDecimal("20.00"));
+
+        // VAT: 20 * 0.23 = 4.60
+        // Cena z VAT: 24.60
+        // Akcyza: 5.56
+        // suma 30.16
+        Assert.assertThat(
+                new BigDecimal("30.16"),
+                Matchers.comparesEqualTo(wine.getPriceWithTax())
+        );
+    }
+
+    @Test
+    public void testFuelCanisterPriceWithExciseOnly() {
+        Product fuel = new FuelCanister("Benzyna", new BigDecimal("7.00"));
+
+        // VAT: 0, netto: 7.00
+        // Akcyza: 5.56
+        // suma 12.56
+        Assert.assertThat(
+                new BigDecimal("12.56"),
+                Matchers.comparesEqualTo(fuel.getPriceWithTax())
+        );
+    }
+
+
 }
